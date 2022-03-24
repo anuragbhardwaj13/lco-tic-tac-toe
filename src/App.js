7aa862e72b175719cbb023e86f060d5dc24d6cfc
Anurag Bhardwaj <a4anurag13@gmail.com>
2022-03-25 01:51:13 +0530

    logic-fixed

--- src/App.js
+++ src/App.js
@@ -67,21 +67,6 @@ const App = () => {
       itemArray[2] !== "empty"
     ) {
       setWinMessage(`${itemArray[2]} Wins`);
-    } else {
-      if (
-        itemArray[0] !== "empty" &&
-        itemArray[1] !== "empty" &&
-        itemArray[2] !== "empty" &&
-        itemArray[3] !== "empty" &&
-        itemArray[4] !== "empty" &&
-        itemArray[5] !== "empty" &&
-        itemArray[6] !== "empty" &&
-        itemArray[7] !== "empty" &&
-        itemArray[8] !== "empty" &&
-        itemArray[9] !== "empty"
-      ) {
-        setWinMessage("No One Won");
-      }
     }
   };
 
